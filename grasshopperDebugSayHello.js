dd2983eb204fe7176eae5f5fa592af10f7c8cce7
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-02-11 00:44:23 -0500

    added grasshapperDebugSayHello.js

---
+++ grasshopperDebugSayHello.js
@@ -0,0 +1,18 @@
+//Date First Completed
+//02-11-23
+
+//Prompt
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+// Example output:
+// Hello, Mr. Spock
+
+//My solution
+const sayHello = (name) => `Hello, ${name}`
+  
+//Favorite Solution
+//const sayHello = name => `Hello, ${name}`
+
+//What I learned
+//If there's only one parameter, it doesn't need parenthesis.
+
