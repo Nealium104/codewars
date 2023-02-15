39d3206ce02d9a239fec909b24b82090214bb6e1
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-02-15 11:40:45 -0500

    forgot to save reversedSequence

--- reversedSequence.js
+++ reversedSequence.js
@@ -1,15 +1,30 @@
 ////////////////////////////////////////////////////////////////////
-// Date Completed
-// KYU
+// Date Completed 02-15-22
+// KYU 8
 
 ////////////////////////////////////////////////////////////////////
 // Prompt
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
 
 // /////////////////////////////////////////////////////////////////
 // My Solution
+// const reverseSeq = n => {
+//     const reverseArray = []
+//     for(i = n; i > 0; i--){
+//       reverseArray.push(i)
+//     }
+//     return reverseArray
+//   };
+
 
 ////////////////////////////////////////////////////////////////////
 // Favorite Solution
+// const reverseSeq = length => Array.from({length}, () => length--)
+
 
 ////////////////////////////////////////////////////////////////////
 // What I learned
+// Slowly building more confidence here. I remembered to initalize an array (last time I had an array problem, I tried using a loop on a string), and got to use my for loop in a different way. 
