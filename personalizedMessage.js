af92703912bf3e33b57d86d4ee63e412f1a37dce
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-03-15 19:42:55 -0400

    added personalmessage.js

---
+++ personalizedMessage.js
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 03-15-23
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+// case 	return
+// name equals owner 	'Hello boss'
+// otherwise 	'Hello guest'
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+const greet = (name, owner) => owner === name ? "Hello boss" : "Hello guest"
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// let
+// greet
+// = ( Ⰾ , Ⰺ ) =>
+// `Hello ${( 
+// ⰎⰀⰊ
+// = (Ⰾ , Ⰺ) =>
+// ''),(  Ⰾ === Ⰺ ?`boss`
+// :     'guest'     )
+// }`;  `Ⰿ´  ;´{`
+
+// /*  DECRYPT ME!  */
+/*  Ⰰ ⰎⰏⰊ Ⰰ  */
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// I remembered to do my ternary!
