c35bf4874f0d20b3fe4c1414ee19fb2d6dc87654
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-12 11:50:02 -0400

    added iLoveYou.js

---
+++ iLoveYou.js
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 04-12-23
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+//     "I love you"
+//     "a little"
+//     "a lot"
+//     "passionately"
+//     "madly"
+//     "not at all"
+
+// If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+function howMuchILoveYou(nbPetals) {
+    let array = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"]
+    return array[(nbPetals - 1) % 6]
+  }
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// const howMuchILoveYou=n=>['not at all','I love you','a little','a lot','passionately','madly'][n%6]
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// This one was a tiny bit tough. I first solved it by making a new variable nbPetalsFixed to deal with the 0 index, but
+// I figured out that I could just do the math in the array. This one was good!
