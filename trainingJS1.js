999ee492c8cbba0078baf0a0cd1c49bb8aaa21c9
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-12 11:35:37 -0400

    added trainingJS1.js

---
+++ trainingJS1.js
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 04-12-23
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// Task
+
+// Please refer to two example above and write your first JS function.
+
+// mission 1:
+
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+// mission 2:
+
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+// mission 3:
+
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+const helloWorld = () => {
+    const str="Hello World!"
+    console.log(str)
+  }
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// function helloWorld() {
+//     var i = 0;
+//     var vаr;
+//     vаr = "var";
+//     var str = 'str'
+//     var summa = "";
+//     const f1 = "H";
+//     const f2 = "e";
+//     const f3 = "l";
+//     const f4 = "l";
+//     const f5 = "o";
+//     const f6 = " ";
+//     const f7 = "W";
+//     const f8 = "o";
+//     const f9 = "r";
+//     const f10 = 'l';
+//     const f11 = 'd';
+//     const f12 = "!";
+//     while (i!=1){
+//       i++;
+//       summa = summa + f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10 + f11 + f12
+//     };
+//     if ("if" === "iF"){
+//       return "Error, press F5"
+//     } else {
+//       console.log(summa)
+//     }
+//   }
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// This one was definitely a give-me. 
