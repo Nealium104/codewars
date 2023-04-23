a05737b89ec5447e7f3671927bdae93e9fea7cab
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-04-23 09:27:16 -0400

    added isItEven.js

---
+++ isItEven.js
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 04-23-23
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+const testEven = n => n % 2 === 0
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// function testEven(n){
+//     return !(n % 2);
+//   }
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// I actually like mine more than favorite. Mine is shorter. This was too easy. I think I need to make my normal 6's and 7's now
+// instead of 7's and 8's
