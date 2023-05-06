caf62559eb83cf907b680b359b4ab74f3637fcce
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-05-06 00:31:13 -0400

    added reverseListOrder.js

---
+++ reverseListOrder.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 04-06-23
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+// Examples (Input -> Output)
+
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+const reverseList = list => list.reverse()
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// function reverseList(list) {
+//     var array = [];
+//     for (var i = list.length-1; i >= 0; i --){
+//       array.push(list[i]);
+//     }
+//     return array;
+//   }
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// The favorite is an interesting way to use a traditional for loop!
