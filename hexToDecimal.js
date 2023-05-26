7cb48034f94b8028a7a78d7fd439311a3adee24b
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-05-26 09:07:01 -0400

    added hexToDecimal.js

---
+++ hexToDecimal.js
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 05-26-23
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+const hexToDec = hexString => parseInt(hexString, 16)
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// function hexToDec(hexString){
+//     let hexVal = {
+//       a: 10,
+//       b: 11,
+//       c: 12,
+//       d: 13,
+//       e: 14,
+//       f: 15,
+//     },  clone = hexString.split('').map(x => x.toLowerCase()), isNeg = false;
+//     if(clone[0] === '-') {
+//       isNeg = true;
+//       clone.splice(0,1);
+//     }
+//     let output = Number(clone
+//       .reverse()
+//       .map((e,i)=> hexVal[e] ? hexVal[e]*Math.pow(16, i) : e*Math.pow(16, i))
+//       .reduce((a,b) => a+b), 10);
+//     return isNeg ? output * -1 : output;
+//   }
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// While the actual coding for this one was easy, I took some time to understand parseInt a little better and memorized what a hexidecimal is
