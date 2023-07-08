2b72994c37a4d593b4f4b773a1fe8d31049699dc
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-07-08 08:51:38 -0400

    added howManyLightsabers.js

--- 1template.js
+++ 1template.js
@@ -1,37 +1,20 @@
 ////////////////////////////////////////////////////////////////////
-// Date Completed 07-03-23
-// KYU 7
+// Date Completed
+// KYU
 
 
 ////////////////////////////////////////////////////////////////////
 // Prompt
-// Challenge:
-
-// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
-
-// Example:
-
-// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
-
-// Addendum:
-
-// Please, keep in mind, that JavaScript is by default sorting objects alphabetically. For more information, please consult:
-
-// http://stackoverflow.com/questions/6093874/why-doesnt-the-sort-function-of-javascript-work-well
 
 
 ///////////////////////////////////////////////////////////////////
 // My Solution
-const flattenAndSort = array => array.flat().sort((a, b) => a - b)
+
 
 ////////////////////////////////////////////////////////////////////
 // Favorite Solution
-// "use strict";
 
-// function flattenAndSort(array) {
-//   return [].concat(...array).sort((a,b) => a - b);
-// }
 
 ////////////////////////////////////////////////////////////////////
 // What I learned
-// The favorite has a neat use of .concat with the spread operator
+
---
+++ howManyLightsabers.js
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 07-08-23
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// Inspired by the development team at Vooza, write the function that
+
+//     accepts the name of a programmer, and
+//     returns the number of lightsabers owned by that person.
+
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+const howManyLightsabersDoYouOwn = name => name === "Zach" ? 18 : 0
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// // TODO: Make some stuff more good here
+// function howManyLightsabersDoYouOwn(R2D2_ListenOrders){
+//     var R2D2_ListenOrders;
+//     var R2D2_Answer;
+//     var His_lightsabacollection;
+//     if (R2D2_ListenOrders=="Zach"){
+//     R2D2_Answer="Bipipipi"
+//     };
+//     if (R2D2_Answer=="Bipipipi"){
+//     his_lightsabacollection=18;
+//     return his_lightsabacollection}
+//     else{
+//     R2D2_Answer=="no beep"
+//     his_lightsabacollection=0;
+//     return his_lightsabacollection;}
+//     };
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// This was pretty easy.
