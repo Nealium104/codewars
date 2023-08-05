f720edda5712c8a8e1ceef38410199f7e524e933
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-08-05 09:02:20 -0400

    added doYouSpeakEnglish.js

--- doYouSpeakEnglish.js
+++ doYouSpeakEnglish.js
@@ -1,3 +1,31 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 08-04-2023
+// KYU 8
+
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+
+///////////////////////////////////////////////////////////////////
+// My Solution
 function spEng(sentence){
-    //write your code here
-    }
+  return sentence.toLowerCase().includes('english')
+}
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+// function spEng(s){
+//    return /english/i.test(s)
+// }
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// Not much was learned here. Was pretty easy.
