f48ba4d942dbdedd574541330ee370833cc73f9f
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-04 11:12:15 -0400

    added basicDataTypes--Object.js

---
+++ basicDataTypes--Object.js
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////
+// Date Completed 09-04-23
+// KYU 8
+
+////////////////////////////////////////////////////////////////////
+// Prompt
+// In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
+
+// You can define the object attributes during initialization, like this:
+
+// var animal={name:"dog"}
+
+// you can also set/get some properties after the object definition, like this:
+
+// var animal={}
+// animal.name="dog"  (or animal["name"]="dog")
+
+// Task
+
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+///////////////////////////////////////////////////////////////////
+// My Solution
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+
+////////////////////////////////////////////////////////////////////
+// Favorite Solution
+
+////////////////////////////////////////////////////////////////////
+// What I learned
+// This one was probably too easy for where I'm at. I did make a little solution that accesses the properties by
+// bracket notation too, so that was good practice.
