b4a48445c64528241b6f090ef1f260cf9b35db09
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-26 12:15:49 -0400

    modified countTheDigit.js

--- countTheDigit.js
+++ countTheDigit.js
@@ -2,7 +2,6 @@
 // Date Completed 07-21-23
 // KYU 7
 
-
 ////////////////////////////////////////////////////////////////////
 // Prompt
 // Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
@@ -14,7 +13,7 @@
 // Implement the function taking n and d as parameters and returning this count.
 // Examples:
 
-// n = 10, d = 1 
+// n = 10, d = 1
 // the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
 // We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
 
@@ -25,20 +24,19 @@
 
 // Note that 121 has twice the digit 1.
 
-
 ///////////////////////////////////////////////////////////////////
 // My Solution
-function nbDig(n, d) {
-    let nums = [];
-    
-    for(let i = 0; i <= n; i++){
-        nums.push(i * i);
-    }
-    
-    let digits = nums.toString().split(',').join('').split('').map(Number);
-    
-    return digits.filter(num => num === d).length;
-}
+const nbDig = (n, d) => {
+  let nums = [];
+
+  for (let i = 0; i <= n; i++) {
+    nums.push(i * i);
+  }
+
+  let digits = nums.toString().split(",").join("").split("").map(Number);
+
+  return digits.filter((num) => num === d).length;
+};
 
 ////////////////////////////////////////////////////////////////////
 // Favorite Solution
