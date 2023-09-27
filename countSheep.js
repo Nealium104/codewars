aebe07d40abbd65347600a1d66c0c6e7d8072449
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-27 12:14:58 -0400

    modified countSheep.js

--- countSheep.js
+++ countSheep.js
@@ -9,22 +9,19 @@
 
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
-
-
 // /////////////////////////////////////////////////////////////////
 // My Solution
-const countSheep = function (num){
-    let dream = ``
-    for(let i = 1; i <= num; i++){
-     dream = dream.concat(`${i} sheep...`)
-    }
-    return dream
+const countSheep = (num) => {
+  let dream = ``;
+  for (let i = 1; i <= num; i++) {
+    dream = dream.concat(`${i} sheep...`);
   }
+  return dream;
+};
 
 ////////////////////////////////////////////////////////////////////
 // Favorite Solution
 // countSheep=n=>[...Array(n).keys()].map(x=>`${x+1} sheep...`).join``
-
 
 ////////////////////////////////////////////////////////////////////
 // What I learned
