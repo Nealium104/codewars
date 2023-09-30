e9c682af50cbe207b5bd5a326846f76e7812bce9
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-30 12:12:24 -0400

    modified convertNum....js

--- convertNumberToReversedArrayOfDigits.js
+++ convertNumberToReversedArrayOfDigits.js
@@ -2,10 +2,9 @@
 // Date Completed - 05/08/23
 // KYU 8
 
-
 ////////////////////////////////////////////////////////////////////
 // Prompt
-// Convert number to reversed array of digits
+// Convert number to reversed array of digits.
 
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 // Example(Input => Output):
@@ -13,26 +12,28 @@
 // 35231 => [1,3,2,5,3]
 // 0 => [0]
 
-
-
 ///////////////////////////////////////////////////////////////////
 // My Solution
 function digitize(n) {
-    return n.toString().split('').reverse().map(num => parseInt(num));
-  }
+  return n
+    .toString()
+    .split("")
+    .reverse()
+    .map((num) => parseInt(num));
+}
 
 ////////////////////////////////////////////////////////////////////
 // Favorite Solution
 // function digitize(n) {
 //     //code here
 //     let numArr = [];
-    
+
 //     while(n > 0){
 //       numArr.push(n % 10);
-      
+
 //       n = Math.floor(n/10);
 //     }
-    
+
 //     return numArr;
 //   }
 
