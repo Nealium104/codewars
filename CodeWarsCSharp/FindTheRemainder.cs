b64ef9be8316c33eabbb6df20f69d15a577d69fc
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-12-30 10:09:18 -0500

    added return.cs

--- CodeWarsCSharp/FindTheRemainder.cs
+++ CodeWarsCSharp/FindTheRemainder.cs
@@ -50,7 +50,7 @@ public class FindTheRemainder
 
 ////////////////////////////////////////////////////////////////////
 // Favorite Solution
-using System;
+// using System;
 
 public class FindTheRemainderFavoriteSolution
 {
---
+++ CodeWarsCSharp/return.cs
@@ -0,0 +1,29 @@
+using System;
+
+public static class Kata
+{
+  public static int Add(int a, int b)
+    {
+       return a + b;
+    }
+  public static int Subt(int a, int b)
+    {
+       return a - b;
+    }
+  public static int Multiply(int a, int b)
+    {
+    return a * b;
+  }
+  public static int Divide(int a, int b)
+    {
+    return a / b;
+  }
+  public static int Mod(int a, int b)
+    {
+    return a % b;
+  }
+  public static double Exponent(int a, int b)
+    {
+    return Math.Pow(a, b);
+  }
+}
